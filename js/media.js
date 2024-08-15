7b2e55cd92b63a538c265868ed98db99eefc40e3
abdullox0900 <abdullox0900@gmail.com>
2024-08-15 14:47:55 +0500

    feat: add footer

--- js/media.js
+++ js/media.js
@@ -1,12 +1,12 @@
-function updateAnimation() {
-    if (window.innerWidth < 1300) {
-        // Mobil qurilmalar uchun
-        gsap.to(".header", { display: 'none' })
-    } else {
-        // // Katta ekranlar uchun
-        // gsap.to(".element", { x: 200, duration: 1 })
-    }
-}
+// function updateAnimation() {
+//     if (window.innerWidth < 1300) {
+//         // Mobil qurilmalar uchun
+//         gsap.to(".header", { display: 'none' })
+//     } else {
+//         // // Katta ekranlar uchun
+//         // gsap.to(".element", { x: 200, duration: 1 })
+//     }
+// }
 
-window.addEventListener("load", updateAnimation)
-window.addEventListener("resize", updateAnimation)
+// window.addEventListener("load", updateAnimation)
+// window.addEventListener("resize", updateAnimation)
